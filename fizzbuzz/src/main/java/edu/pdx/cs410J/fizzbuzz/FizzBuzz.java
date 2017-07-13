bbdd73af744234f497070487875c9f4761870b16
David Whitlock <david.m.whitlock@gmail.com>
2017-07-12 19:16:29 -0700

    Remove last vestiges of airline archetype.

--- fizzbuzz/src/main/java/edu/pdx/cs410J/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/edu/pdx/cs410J/fizzbuzz/FizzBuzz.java
@@ -1,30 +1,5 @@
 package edu.pdx.cs410J.fizzbuzz;
 
-import edu.pdx.cs410J.AbstractFlight;
+public class FizzBuzz {
 
-public class FizzBuzz extends AbstractFlight {
-  @Override
-  public int getNumber() {
-    return 42;
-  }
-
-  @Override
-  public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getDepartureString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
-
-  @Override
-  public String getArrivalString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
-  }
 }
--- fizzbuzz/src/test/java/edu/pdx/cs410J/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/edu/pdx/cs410J/fizzbuzz/FizzBuzzTest.java
@@ -1,31 +1,9 @@
 package edu.pdx.cs410J.fizzbuzz;
 
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 /**
  * Unit tests for the {@link FizzBuzz} class.
  */
 public class FizzBuzzTest {
   
-  @Test(expected = UnsupportedOperationException.class)
-  public void getArrivalStringNeedsToBeImplemented() {
-    FizzBuzz flight = new FizzBuzz();
-    flight.getArrivalString();
-  }
 
-  @Test
-  public void initiallyAllFlightsHaveTheSameNumber() {
-    FizzBuzz flight = new FizzBuzz();
-    assertThat(flight.getNumber(), equalTo(42));
-  }
-
-  @Test
-  public void forProject1ItIsOkayIfGetDepartureTimeReturnsNull() {
-    FizzBuzz flight = new FizzBuzz();
-    assertThat(flight.getDeparture(), is(nullValue()));
-  }
-  
 }
