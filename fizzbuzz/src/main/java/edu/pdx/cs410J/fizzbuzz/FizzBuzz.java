adc67a5544f336dbd25db5ff4c7389f0a968a0d0
David Whitlock <david.m.whitlock@gmail.com>
2017-07-12 19:38:33 -0700

    Implement the logic for FizzBuzz.

--- fizzbuzz/src/main/java/edu/pdx/cs410J/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/edu/pdx/cs410J/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,20 @@ package edu.pdx.cs410J.fizzbuzz;
 
 public class FizzBuzz {
 
+  static final String FIZZ = "Fizz";
+  static final String BUZZ = "Buzz";
+  static final String FIZZBUZZ = "FizzBuzz";
+
+
+  public static String fizzBuzz(int number) {
+    if (number % 15 == 0)
+      return FIZZBUZZ;
+    else if(number % 3 == 0) {
+      return FIZZ;
+    } else if (number % 5 == 0) {
+      return BUZZ;
+    } else {
+      return String.valueOf(number);
+    }
+  }
 }
--- fizzbuzz/src/test/java/edu/pdx/cs410J/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/edu/pdx/cs410J/fizzbuzz/FizzBuzzTest.java
@@ -1,9 +1,36 @@
 package edu.pdx.cs410J.fizzbuzz;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * Unit tests for the {@link FizzBuzz} class.
  */
 public class FizzBuzzTest {
-  
 
+  private void assertThatFizzBuzzOfNumberEquals(int number, String expected) {
+    assertThat(FizzBuzz.fizzBuzz(number), equalTo(expected));
+  }
+
+  @Test
+  public void fizzBuzzOf1Is1() {
+    assertThatFizzBuzzOfNumberEquals(1, "1");
+  }
+
+  @Test
+  public void fizzBuzzOf3isFizzI() {
+    assertThatFizzBuzzOfNumberEquals(3, FizzBuzz.FIZZ);
+  }
+
+  @Test
+  public void fizzBuzzOf5IsBuzz() {
+    assertThatFizzBuzzOfNumberEquals(5, FizzBuzz.BUZZ);
+  }
+
+  @Test
+   public void fizzBuzzOf15IsFizzBuzz() {
+     assertThatFizzBuzzOfNumberEquals(15, FizzBuzz.FIZZBUZZ);
+   }
 }
