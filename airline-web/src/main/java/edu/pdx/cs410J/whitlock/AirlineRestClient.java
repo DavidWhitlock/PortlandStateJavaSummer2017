0de027139af6d6e3adfe53224a0d4d1b6668784b
David Whitlock <david.m.whitlock@gmail.com>
2017-07-19 20:28:56 -0700

    Added airline web application for Project 4.

---
+++ airline-web/src/main/java/edu/pdx/cs410J/whitlock/AirlineRestClient.java
@@ -0,0 +1,80 @@
+package edu.pdx.cs410J.whitlock;
+
+import com.google.common.annotations.VisibleForTesting;
+import edu.pdx.cs410J.web.HttpRequestHelper;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+/**
+ * A helper class for accessing the rest client.  Note that this class provides
+ * an example of how to make gets and posts to a URL.  You'll need to change it
+ * to do something other than just send key/value pairs.
+ */
+public class AirlineRestClient extends HttpRequestHelper
+{
+    private static final String WEB_APP = "airline";
+    private static final String SERVLET = "flights";
+
+    private final String url;
+
+
+    /**
+     * Creates a client to the airline REST service running on the given host and port
+     * @param hostName The name of the host
+     * @param port The port
+     */
+    public AirlineRestClient( String hostName, int port )
+    {
+        this.url = String.format( "http://%s:%d/%s/%s", hostName, port, WEB_APP, SERVLET );
+    }
+
+    /**
+     * Returns all keys and values from the server
+     */
+    public Map<String, String> getAllKeysAndValues() throws IOException {
+      Response response = get(this.url);
+      return Messages.parseKeyValueMap(response.getContent());
+    }
+
+    /**
+     * Returns the value for the given key
+     */
+    public String getValue(String key) throws IOException {
+      Response response = get(this.url, "key", key);
+      throwExceptionIfNotOkayHttpStatus(response);
+      String content = response.getContent();
+      return Messages.parseKeyValuePair(content).getValue();
+    }
+
+    public void addKeyValuePair(String key, String value) throws IOException {
+      Response response = postToMyURL("key", key, "value", value);
+      throwExceptionIfNotOkayHttpStatus(response);
+    }
+
+    @VisibleForTesting
+    Response postToMyURL(String... keysAndValues) throws IOException {
+      return post(this.url, keysAndValues);
+    }
+
+    public void removeAllMappings() throws IOException {
+      Response response = delete(this.url);
+      throwExceptionIfNotOkayHttpStatus(response);
+    }
+
+    private Response throwExceptionIfNotOkayHttpStatus(Response response) {
+      int code = response.getCode();
+      if (code != HTTP_OK) {
+        throw new AppointmentBookRestException(code);
+      }
+      return response;
+    }
+
+    private class AppointmentBookRestException extends RuntimeException {
+      public AppointmentBookRestException(int httpStatusCode) {
+        super("Got an HTTP Status Code of " + httpStatusCode);
+      }
+    }
+}
