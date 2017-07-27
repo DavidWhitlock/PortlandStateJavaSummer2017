0660ce219a16fd7719b5f2c5d80421bbd81759ba
David Whitlock <david.m.whitlock@gmail.com>
2017-07-26 20:46:35 -0700

    Added GWT files from archetype

---
+++ airline-gwt/src/main/java/edu/pdx/cs410J/whitlock/server/AirlineServiceImpl.java
@@ -0,0 +1,41 @@
+package edu.pdx.cs410J.whitlock.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import edu.pdx.cs410J.whitlock.client.Airline;
+import edu.pdx.cs410J.whitlock.client.Flight;
+import edu.pdx.cs410J.whitlock.client.AirlineService;
+
+/**
+ * The server-side implementation of the Airline service
+ */
+public class AirlineServiceImpl extends RemoteServiceServlet implements AirlineService
+{
+  @Override
+  public Airline getAirline() {
+    Airline airline = new Airline();
+    airline.addFlight(new Flight());
+    return airline;
+  }
+
+  @Override
+  public void throwUndeclaredException() {
+    throw new IllegalStateException("Expected undeclared exception");
+  }
+
+  @Override
+  public void throwDeclaredException() throws IllegalStateException {
+    throw new IllegalStateException("Expected declared exception");
+  }
+
+  /**
+   * Log unhandled exceptions to standard error
+   *
+   * @param unhandled
+   *        The exception that wasn't handled
+   */
+  @Override
+  protected void doUnexpectedFailure(Throwable unhandled) {
+    unhandled.printStackTrace(System.err);
+    super.doUnexpectedFailure(unhandled);
+  }
+}
