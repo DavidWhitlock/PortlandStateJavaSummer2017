75c8260c85dccfa9e8fe3dc082f0832579137a1b
David Whitlock <david.m.whitlock@gmail.com>
2017-08-09 18:49:16 -0700

    Use a DateTimeFormat to format dates on the client side.

--- airline-gwt/src/main/java/edu/pdx/cs410J/whitlock/client/PrettyPrinter.java
+++ airline-gwt/src/main/java/edu/pdx/cs410J/whitlock/client/PrettyPrinter.java
@@ -1,9 +1,11 @@
 package edu.pdx.cs410J.whitlock.client;
 
+import com.google.gwt.i18n.shared.DateTimeFormat;
 import edu.pdx.cs410J.AirlineDumper;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Date;
 
 public class PrettyPrinter implements AirlineDumper<Airline> {
 
@@ -12,11 +14,26 @@ public class PrettyPrinter implements AirlineDumper<Airline> {
   @Override
   public void dump(Airline airline) throws IOException {
     prettyText.append(airline.toString());
+    prettyText.append("\n");
+
     Collection<Flight> flights = airline.getFlights();
     for (Flight flight : flights) {
-      prettyText.append(flight);
-      prettyText.append("\n");
+      prettyFlight(flight);
     }
+  }
+
+  private void prettyFlight(Flight flight) {
+    prettyText.append("  Flight ").append(flight.getNumber()).append("\n");
+    prettyText.append("      Departing ").append(flight.getSource()).append("\n");
+    prettyText.append("        At ").append(prettyDate(flight.getDeparture())).append("\n");
+    prettyText.append("      Arriving ").append(flight.getDestination()).append("\n");
+    prettyText.append("        At ").append(prettyDate(flight.getArrival())).append("\n");
+    prettyText.append("\n");
+  }
+
+  private String prettyDate(Date date) {
+    DateTimeFormat format = DateTimeFormat.getFormat("MM/dd/yyyy hh:mm a");
+    return format.format(date);
   }
 
   public String getPrettyText() {
